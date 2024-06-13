36f6efb0ad8cd92ad0cf7d0ca41a6e81f98de60c
yoshi <yoshihiro.kanai@neteleven.de>
2024-06-13 09:50:24 +0200

    change in parent for subtree

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -5,14 +5,9 @@ import Button from './Button'
 const meta = {
   title: 'Components/Button',
   component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  parameters: {},
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {},
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: {
     position: 'center-left',
     label: 'Button',
