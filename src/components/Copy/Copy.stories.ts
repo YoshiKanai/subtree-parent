f1fcbf4aa5b7aac3dd56a3f91df0f418c3e9ce1a
yoshi <yoshihiro.kanai@neteleven.de>
2024-07-25 17:53:17 +0200

    removed unused

--- src/components/Copy/Copy.stories.ts
+++ src/components/Copy/Copy.stories.ts
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { fn } from '@storybook/test'
 import Copy from './Copy'
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
